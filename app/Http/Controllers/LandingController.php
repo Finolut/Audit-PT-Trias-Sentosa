ed75fc1a7d768d99e6b3109d254845f88ca9c16e
Lutfino Athallah Pratama <finolut@gmail.com>
2026-01-21 11:05:59 +0700

    update patch 1.1.60

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Department;
 
-class DashboardController extends Controller
+class LandingController extends Controller
 {
 public function landing()
 {
