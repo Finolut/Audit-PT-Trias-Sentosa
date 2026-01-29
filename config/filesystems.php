c081dd5f7637f5b8699b27c1995646887d020580
Lutfino Athallah Pratama <finolut@gmail.com>
2026-01-29 11:59:53 +0700

    update patch 1.4.83

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,19 +47,14 @@ return [
             'report' => false,
         ],
 
-     's3' => [
+ 's3' => [
     'driver' => 's3',
     'key' => env('AWS_ACCESS_KEY_ID'),
     'secret' => env('AWS_SECRET_ACCESS_KEY'),
-
-    // ⬇️ WAJIB GANTI INI
     'region' => env('AWS_REGION'),
-
     'bucket' => env('AWS_BUCKET'),
     'endpoint' => env('AWS_ENDPOINT'),
     'use_path_style_endpoint' => true,
-
-    // ⬇️ WAJIB UNTUK DEBUG
     'throw' => true,
 ],
 
